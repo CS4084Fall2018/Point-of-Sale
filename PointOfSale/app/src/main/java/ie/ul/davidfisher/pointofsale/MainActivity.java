2640f23baab2610d9aa34412fea239b1d2ab74f4
David Fisher <fisherds@rose-hulman.edu>
2018-09-26 13:29:27 +0100

    Context menu

--- PointOfSale/app/src/main/java/ie/ul/davidfisher/pointofsale/MainActivity.java
+++ PointOfSale/app/src/main/java/ie/ul/davidfisher/pointofsale/MainActivity.java
@@ -10,12 +10,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.ContextMenu;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.GregorianCalendar;
@@ -35,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
     mNameTextView = findViewById(R.id.name_text);
     mQuantityTextView = findViewById(R.id.quantity_text);
     mDateTextView = findViewById(R.id.date_text);
+
+    registerForContextMenu(mNameTextView);
 
     mItems.add(new Item("Example 1", 30, new GregorianCalendar()));
     mItems.add(new Item("Example 2", 40, new GregorianCalendar()));
@@ -100,6 +104,25 @@ public class MainActivity extends AppCompatActivity {
     // Boilerplate code. Don't mess with it.
     getMenuInflater().inflate(R.menu.menu_main, menu);
     return true;
+  }
+
+  @Override
+  public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+    super.onCreateContextMenu(menu, v, menuInfo);
+    getMenuInflater().inflate(R.menu.menu_context, menu);
+  }
+
+  @Override
+  public boolean onContextItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_edit:
+        Toast.makeText(this, "TODO Edit", Toast.LENGTH_SHORT).show();;
+        return true;
+      case R.id.action_remove:
+        Toast.makeText(this, "TODO Remove", Toast.LENGTH_SHORT).show();;
+        return true;
+    }
+    return super.onContextItemSelected(item);
   }
 
   @Override
